ca3684ae60273caf79a4ad057a70528016c258b9
voidsonu <sonudragneel4@gmail.com>
2021-12-18 18:20:35 +0530

    chages in about

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const AboutUs = () => {
     return (
         <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit architecto corrupti, praesentium culpa illum, veritatis ad dicta commodi quidem animi, deserunt aliquam? Eius soluta earum animi veniam quibusdam deleniti vitae facere mollitia quidem doloremque. Repellat, quo. Quia quasi recusandae ipsum soluta animi dolorem laboriosam praesentium facilis omnis blanditiis ut mollitia fugiat nam ab rerum unde quis, a, ducimus numquam quam architecto? Officiis provident aperiam distinctio nostrum error suscipit debitis autem ullam magni mollitia, fugiat omnis sit tempore, neque illo facere, optio dolores exercitationem nobis. Fugiat ipsa adipisci et odio provident quisquam voluptatibus debitis impedit? Ducimus voluptate amet est voluptates delectus?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. culpa illum, veritatis ad dicta commodi quidem animi, deserunt aliquam? Eius soluta earum animi veniam quibusdam deleniti vitae facere mollitia quidem doloremque. Repellat, quo. Quia quasi recusandae ipsum soluta animi dolorem laboriosam praesentium facilis omnis blanditiis ut mollitia fugiat nam ab rerum unde quis, a, ducimus numquam quam architecto? Officiis provident aperiam distinctio nostrum error suscipit debitis autem ullam magni mollitia, fugiat omnis sit tempore, neque illo facere, optio dolores exercitationem nobis. Fugiat ipsa adipisci et odio provident quisquam voluptatibus debitis impedit? Ducimus voluptate amet est voluptates delectus?
         </div>
     )
 }
